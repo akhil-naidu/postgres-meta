23d5957b6ba404bcf2230817d715dc9106c58997
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-01-16 10:21:45 +0530

    chore: added encryption.

--- src/lib/cryptoAES.js
+++ src/lib/cryptoAES.js
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js'
 console.log(
   'x-connection-encrypted:',
   CryptoJS.AES.encrypt(
-    'postgresql://postgres:9TIDh5tClzkfCnToShjQ@containers-us-west-191.railway.app:7658/railway',
+    'postgresql://postgres:5wBneBK7LEA8SypB@db.bzfrjzvsapgipjatiyrv.supabase.co:5432/postgres',
     'SAMPLE_KEY'
   ).toString()
 )
